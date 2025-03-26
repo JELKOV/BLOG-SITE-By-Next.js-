09be00699063fa6a3bab46b40f3ff4101b64f667
JAEHO AHN <ajh4234@gmail.com>
2025-03-26 22:20:54 +0900

    #2 component 구성

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import { Fragment } from "react";
+
+import Hero from '../components/home-page/hero';
+import FeaturedPosts from '../components/home-page/featured-posts';
+
+
 function HomePage() {
+    <Fragment>
+        <Hero />
+        <FeaturedPosts />
+    </Fragment>
 
 }
 
 export default HomePage;
+
+// 1) Hero => Present ourselves
+// 2) Featured Posts
