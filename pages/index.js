9ad0ad986ec5e5c2a73dc9bd27fdf390c2445058
JAEHO AHN <ajh4234@gmail.com>
2025-03-27 18:09:28 +0900

    #7: FeaturedPosts → PostItem 연결 및 더미 게시물 렌더링

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,45 @@ import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS는 프로덕션용 React 프레임워크 - 풀스택 React 앱과 사이트를 서버 사이드 렌더링을 통해 쉽게 만들 수 있다",
+    date: "2025-03-25",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
+    date: "2025-03-25",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
+    date: "2025-03-25",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
+    date: "2025-03-25",
+  },
+];
+
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
 
 export default HomePage;
-
-// 1) Hero => Present ourselves
-// 2) Featured Posts
