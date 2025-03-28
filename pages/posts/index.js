2f4988fbded6f003c3f15cf91bf290f63d4a067a
JAEHO AHN <ajh4234@gmail.com>
2025-03-28 15:25:10 +0900

    #8: All Posts 페이지 구현 – 모든 게시물 출력

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,39 @@
-function AllPostsPage() {
+import AllPosts from "../../components/posts/all-posts";
 
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS는 프로덕션용 React 프레임워크 - 풀스택 React 앱과 사이트를 서버 사이드 렌더링을 통해 쉽게 만들 수 있다",
+    date: "2025-03-25",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
+    date: "2025-03-25",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
+    date: "2025-03-25",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
+    date: "2025-03-25",
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
 }
 
 export default AllPostsPage;
