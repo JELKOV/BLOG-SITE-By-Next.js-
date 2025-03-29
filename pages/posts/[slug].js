dda916ad7491291a076cd480822dbbb38c9f88c4
JAEHO AHN <ajh4234@gmail.com>
2025-03-29 18:37:27 +0900

    #12 단일 게시물 데이터 패칭 및 동적경로 처리

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,37 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-function PostDetailPage() {
-    return <PostContent />
+function PostDetailPage(props) {
+  return <PostContent post={props.post} />;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
+
+  const paths = slugs.map((slug) => ({
+    params: { slug: slug },
+  }));
+
+  return {
+    paths,
+    fallback: false, // 모든 경로를 사전 생성 (즉시 로드됨)
+  };
 }
 
 export default PostDetailPage;
