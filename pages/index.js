1e5427b8a1313e5cb83de602adfcbc5ddcc85d16
JAEHO AHN <ajh4234@gmail.com>
2025-03-29 17:14:59 +0900

    #11 getAllPosts & getFeaturedPosts 적용 완료

--- pages/index.js
+++ pages/index.js
@@ -2,46 +2,24 @@ import { Fragment } from "react";
 
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeaturedPosts } from '../lib/posts-util';
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS는 프로덕션용 React 프레임워크 - 풀스택 React 앱과 사이트를 서버 사이드 렌더링을 통해 쉽게 만들 수 있다",
-    date: "2025-03-25",
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
-    date: "2025-03-25",
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
-    date: "2025-03-25",
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
-    date: "2025-03-25",
-  },
-];
-
-function HomePage() {
+function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+  return {
+    props: {
+      posts: featuredPosts
+    }
+  };
 }
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,39 +1,18 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS는 프로덕션용 React 프레임워크 - 풀스택 React 앱과 사이트를 서버 사이드 렌더링을 통해 쉽게 만들 수 있다",
-    date: "2025-03-25",
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
-    date: "2025-03-25",
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
-    date: "2025-03-25",
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJS는 프로덕션용 React 프레임워크...",
-    date: "2025-03-25",
-  },
-];
+function AllPostsPage(props) {
+  return <AllPosts posts={props.posts} />;
+}
 
-function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts
+    }
+  }
 }
 
 export default AllPostsPage;
